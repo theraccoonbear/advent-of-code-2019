7bc437d0250b5fc8ef5cc9a2802c554f6d301694
Don Smith <Don Smith>
2019-12-01 22:20:29 -0600

    problem 2 solution

---
+++ advent-d01p02.js
@@ -0,0 +1,34 @@
+const cbfs = require('fs');
+const util = require('util');
+
+const fs = {
+  readdir: util.promisify(cbfs.readdir),
+  readFile: util.promisify(cbfs.readFile),
+  writeFile: util.promisify(cbfs.writeFile),
+};
+
+function fuelNeeds(mass) {
+  const need = Math.floor(mass / 3) - 2;
+  return need + (need >= 9 ? fuelNeeds(need) : 0);
+}
+
+async function main() {
+  const raw = await fs.readFile('module_weights.json', { encoding: 'utf8' });
+  const module_weights = JSON.parse(raw);
+  console.log("Module Weights:", module_weights);
+  const fuel_needed = module_weights
+    .map(m => Math.floor(m / 3) - 2);
+  // console.log("Fuel Needed:", fuel_needed);
+  const total_fuel_needed = fuel_needed.reduce((prev, cur) => {
+    return prev + cur;
+  }, 0);
+  console.log("Total Fuel Needed:", total_fuel_needed);
+
+  const fuel_needed_recursed = module_weights.map(fuelNeeds);
+  const total_fuel_needed_recursed = fuel_needed_recursed.reduce((prev, cur) => {
+    return prev + cur;
+  }, 0);
+  console.log("Total Fuel Needed (Recursed):", total_fuel_needed_recursed);
+}
+
+main();
